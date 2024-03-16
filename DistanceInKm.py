6014558e9e860ec42aa7d4d9b4da58740129d84e
Wlliam <146653695+Githinji0@users.noreply.github.com>
2024-03-16 11:12:15 +0300

    Add files via upload

---
+++ DistanceInKm.py
@@ -0,0 +1,4 @@
+DistanceInMiles = input("enter distance in miles: ")
+DistanceInKms = int(DistanceInMiles) / 1.65
+
+print(DistanceInKms)
---
+++ MoreInputs.py
@@ -0,0 +1,5 @@
+BirthYear = input("birth year: ") 
+age = 2024 - int(BirthYear)
+
+print(age)
+print(type(age))
