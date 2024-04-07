a51782881f9ae5aa2a19fb4a03b76c4a261f2880
WilliamGithinji <146653695+Githinji0@users.noreply.github.com>
2024-04-07 16:35:34 +0300

    Add files via upload

---
+++ pyProg/Converters.py
@@ -0,0 +1,6 @@
+def poundsToKgs(weight):
+    return weight * 0.45
+
+
+def kgsToPounds(weight):
+    return weight / 0.45
---
+++ pyProg/main.py
@@ -0,0 +1,4 @@
+import Converters
+from Converters import kgsToPounds
+kgsToPounds(90)
+print(Converters.kgsToPounds(50))
